efdaab0e804c988988b18b268ba0e6e279d7c43f
Nihar R Panda <110398042+NiharPanda23@users.noreply.github.com>
2023-05-01 12:52:36 +0530

    Project initiated

---
+++ frontend_react/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App;
---
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SkillS.scss'
+
+const Skills = () => {
+  return (
+    <div>Skills</div>
+  )
+}
+
+export default Skills
