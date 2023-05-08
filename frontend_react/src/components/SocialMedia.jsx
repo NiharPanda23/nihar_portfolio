df43c7db55454e5612ec418d0251d983474a497c
Nihar R Panda <110398042+NiharPanda23@users.noreply.github.com>
2023-05-08 21:13:53 +0530

    Website Completed

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -5,13 +5,19 @@ const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div>
+          <a href='https://www.instagram.com/'>
             <BsInstagram/>
+          </a>
         </div>
         <div>
+          <a href='https://github.com/NiharPanda23'>
             <FaGithub/>
+          </a>
         </div>
         <div>
+          <a href='https://www.linkedin.com/in/nihar-ranjanpanda/'>
             <FaLinkedinIn/>
+          </a>
         </div>
     </div>
   )
